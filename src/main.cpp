e7881067a086406a2661a3eb23810766c456c6f6
Tim Lagnese <timlag1305@gmail.com>
2016-10-11 11:16:52 -0400

    Add room number to device provisioning

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,14 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>     
 
-
 void setup()
 {
     Serial.begin(115200);
     WiFiManager wifiManager;
-    wifiManager.autoConnect();
-    Serial.println("Connected!");
+    WiFiManagerParameter roomNum("room_num", "Room Number", "", 4);
+    wifiManager.addParameter(&roomNum);
+    wifiManager.startConfigPortal("ESP_AP");
+    int room = (int) roomNum.getValue();
 }
 
 void loop()
